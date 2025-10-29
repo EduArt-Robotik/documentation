1aaf3365a0bb5d80873263f8569dbc526efd3c25
Sina Steinmüller <sina_steinmller@gmx.de>
2025-10-29 11:22:42 +0100

    changed baseURL to /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   url: 'https://test.eduart-robotik.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
